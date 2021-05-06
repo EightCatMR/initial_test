9a4cc20bdceaa516ee4e1dbd69c0c302528fde65
Roman <48865180+EightCatMR@users.noreply.github.com>
2021-05-06 23:05:24 +0500

    Update Задание1.py

--- Задание1.py
+++ Задание1.py
@@ -7,8 +7,14 @@ def get_order(arr):
     order = np.argsort(angles)
     return np.append(order[0], np.flip(order[1:]))
 
+def get_radius(arr):
+    return np.sqrt(np.power(arr[:, 0], 2) + np.power(arr[:, 1], 2))
 n = input("Введите размер массива с точками на декартовой плоскости: ")
 
 points = np.random.randint(-100, 100, (int(n), 2))
 order = get_order(points)
+radiuses = get_radius(points)
+
 print("Расположение точек в требуемом порядке: {}".format(points[order].tolist()))
+print(f"Среднее значение радиуса: {np.mean(radiuses)}")
+print(f"Минимальное значение радиуса: {np.min(radiuses)}; \nМаксимальное значение радиуса: {np.max(radiuses)}")
